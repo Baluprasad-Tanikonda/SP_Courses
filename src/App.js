bc6f106579a4354f5fb2c55484636aa4367202b6
Baluprasad-Tanikonda <tanikonndabalu1707@gmail.com>
2024-08-05 20:58:59 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Course from './components/Courses_category/Course';
+
+const App = () => {
+
+  return (
+    <div>
+      <Course/>
+    </div>
+  );
+};
+
+export default App;
